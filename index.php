7189b8e2901bfa2ee450c48ffec97ba768d830ca
dilip baghel <dilip.18baghel@gmail.com>
2022-12-15 19:47:02 +0530

    created the category page

--- index.php
+++ index.php
@@ -10,14 +10,83 @@
 
     <?php include "partials/_header.php" ?> 
     
-    <h2 class="text-center">Welcome to iDiscuss</h2>
+    <h1 class="text-center my-3">Welcome to iDiscuss</h1>
     <div class="container">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora minima delectus officiis recusandae qui fugiat sequi itaque doloremque vitae aperiam et obcaecati, facilis magni laborum totam praesentium maiores cum dignissimos, harum doloribus, molestiae accusamus nostrum! Corporis ullam error omnis possimus quas magnam exercitationem repellendus numquam laboriosam eos. Labore assumenda minima neque fuga. Totam eius ullam omnis provident voluptatibus, ex voluptates tempore sapiente recusandae culpa amet velit? Voluptatibus mollitia ducimus accusamus qui libero consequatur nisi fuga facilis nemo. Eaque mollitia ad architecto hic modi doloremque accusantium labore totam id obcaecati, maxime voluptatibus praesentium perspiciatis repudiandae nulla, minima at? Facere neque consectetur dolorum aperiam, deleniti totam quia aspernatur quaerat deserunt repudiandae nisi, animi ut id provident sunt consequatur quidem placeat dolor. Nisi amet aspernatur fugit totam et vitae nobis culpa porro, quod eos officiis, ab fugiat rerum accusantium reprehenderit unde consectetur at ullam, a qui. Eveniet, eos? Nemo ipsa inventore accusantium aspernatur sint! Sunt minus sequi porro commodi hic, reprehenderit iste aliquam, voluptate neque ab placeat alias facere repellat. Veniam illo natus totam officiis non et suscipit, perferendis tempore accusantium sunt numquam doloremque ipsum voluptas, quia dolorum ipsam tenetur delectus? Ratione, sapiente illum! Deserunt incidunt, nostrum soluta reiciendis asperiores tempora delectus dolorum quam earum possimus illo assumenda quia maxime adipisci dolorem a alias perspiciatis ullam mollitia voluptas at fugit quisquam ut! Porro quisquam beatae asperiores eaque nam tenetur nulla veritatis rem ad nihil accusamus explicabo quia, itaque, necessitatibus amet aspernatur optio ut deserunt corrupti dolorem eligendi quo, autem saepe? Inventore ducimus atque, iusto quis recusandae voluptatum fugiat obcaecati similique repudiandae quam dicta aut laudantium aperiam perferendis officia temporibus vero delectus consequatur illo consectetur corrupti dolores voluptas quo? Ullam, eos aspernatur. Facilis numquam ut necessitatibus hic doloremque sit neque quos delectus repellendus, commodi porro deleniti rerum vel nemo, ratione, totam tempore molestiae recusandae qui temporibus dicta. Rem corrupti asperiores dignissimos sunt illum deleniti in hic, sint, voluptate, quis suscipit nisi adipisci </p>
+      <div class="row">
 
-        <p>animi assumenda? Architecto recusandae accusantium pariatur nihil eius voluptates possimus accusamus hic sint fugiat modi vitae provident earum quo porro, illo, quos numquam non perferendis eos error voluptatum. Earum nostrum, a similique, corporis enim placeat quas doloribus ex expedita modi quod beatae illo! Harum cum, excepturi aliquid culpa blanditiis quis molestias repudiandae illo deserunt eveniet error sunt doloremque repellendus vitae assumenda cumque enim maxime consectetur et dignissimos sint eius odio? Corporis, assumenda explicabo. Corporis culpa nostrum debitis asperiores neque autem, itaque quia quae cupiditate libero mollitia et tenetur unde ab optio nemo sapiente veniam quasi omnis est? Laboriosam molestiae minima modi nesciunt, cupiditate voluptatem similique aliquam unde architecto molestias fugiat quis provident recusandae dolore optio perspiciatis temporibus quaerat dolorem? Minima ut molestiae praesentium obcaecati quisquam at recusandae odio optio omnis, ad odit ex possimus! Natus iusto dicta quibusdam omnis, nobis doloremque dolorem ea? Reiciendis quod molestiae, quam odio quis, fuga consectetur dignissimos voluptate nemo magnam labore provident necessitatibus esse quaerat doloremque voluptatum animi incidunt tempora nostrum aliquam minus rem eum? Placeat consequatur aperiam cupiditate eius ratione id omnis ipsam iusto officiis atque.</p>
+        <div class="col-md-4 my-3">
+          <div class="card" style="width: 18rem;">
+              <img src="images/python.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Python</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">View threads</a>
+              </div>
+          </div>        
+      </div>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, molestias, cupiditate autem nobis aliquid consectetur optio nemo repudiandae, nihil quidem modi! Error excepturi pariatur asperiores, illum cupiditate ducimus unde, libero officia officiis atque consectetur eos adipisci harum, perspiciatis accusantium provident dignissimos! Quis quisquam sapiente iusto, facilis, cumque distinctio rerum modi facere voluptatum eligendi temporibus repellendus, quae pariatur ipsa optio voluptatem veritatis est libero hic! Quod ipsum distinctio expedita incidunt error temporibus quo illum tempore nemo ea eos excepturi, esse amet dolorum molestias fuga! Voluptate illum deserunt voluptates, quidem quos, corrupti doloribus sunt consectetur ipsa necessitatibus eligendi ducimus tempora praesentium. Eveniet ducimus aut, laborum eligendi eos veritatis rem! Earum aliquam placeat dolorem iusto odit odio eum? Fuga cum officiis veniam debitis laudantium omnis aliquam magni sint, consequuntur non, quos, doloribus illum. Officiis nobis deleniti assumenda nostrum harum voluptatibus, ab eum odit eaque cum fuga est eos ea in provident maiores reprehenderit quia suscipit consequatur dolores! Placeat minus, modi quaerat voluptatum consequuntur ab atque odio, natus reprehenderit iste deleniti pariatur in eaque totam est voluptates! Omnis at enim ipsa pariatur nam, dolores atque vitae iure beatae dolorum officiis repellat. Consequuntur dolor fugit aliquam vel voluptate reprehenderit architecto perferendis rerum optio laudantium totam, dolores ad inventore, eos labore. Quasi distinctio possimus modi eum, labore quam. Dignissimos nam at quam ullam natus magni sed, cumque, saepe eaque eveniet fugiat! Magnam perferendis natus voluptatem delectus modi, sit tempore earum, at tenetur ipsa cupiditate incidunt culpa eum! Dolores, et quas debitis tenetur voluptatum, praesentium voluptas, doloremque expedita accusantium labore quam alias maiores laborum minima eaque doloribus dicta aliquam fugit. Assumenda expedita porro quos error perferendis nihil? Modi aperiam mollitia ratione iure voluptatum consequuntur, maiores asperiores minus dignissimos eligendi architecto ea iste repudiandae fugit provident doloribus tempore, perferendis amet libero distinctio quidem! Officia illo suscipit dolores placeat?</p>
+
+      <div class="col-md-4 my-3">
+          <div class="card" style="width: 18rem;">
+              <img src="images/python.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Python</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">View threads</a>
+              </div>
+          </div>        
+      </div>
+
+
+      <div class="col-md-4 my-3">
+          <div class="card" style="width: 18rem;">
+              <img src="images/python.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Python</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">View threads</a>
+              </div>
+          </div>        
+      </div>
+
+
+      <div class="col-md-4 my-3">
+          <div class="card" style="width: 18rem;">
+              <img src="images/python.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Python</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">View threads</a>
+              </div>
+          </div>        
+      </div>
+
+
+      <div class="col-md-4 my-3">
+          <div class="card" style="width: 18rem;">
+              <img src="images/python.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Python</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">View threads</a>
+              </div>
+          </div>        
+      </div>
+
+
+      <div class="col-md-4 my-3">
+          <div class="card" style="width: 18rem;">
+              <img src="images/python.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Python</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">View threads</a>
+              </div>
+          </div>        
+      </div>
+
     </div>
+  </div>
     <?php include "partials/_footer.php" ?> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
